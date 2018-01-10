b17b4c829fa24541cc9b72bdc964fad532ec4871
Jonathan Helmus <jjhelmus@gmail.com>
2018-01-10 16:38:11 -0600

    clean up build script
    * LDFLAGS and CFLAGS are set in compiler activate script
    * build in parallel

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-export LDFLAGS=-L$PREFIX/lib 
-export CFLAGS=-I$PREFIX/include
 ./configure --with-gssapi-impl=mit --prefix=$PREFIX
-make
+make -j${CPU_COUNT}
 make check
 make install
