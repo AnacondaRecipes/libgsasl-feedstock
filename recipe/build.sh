47d5e60940920f9fac4eb2b7e4d1795b233df29d
Kai Tietz <ktietz@anaconda.com>
2021-09-02 11:38:38 +0200

    add osx-arm64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./build-aux
+
 ./configure --with-gssapi-impl=mit --prefix=$PREFIX --build=${BUILD} --host=${HOST}
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
